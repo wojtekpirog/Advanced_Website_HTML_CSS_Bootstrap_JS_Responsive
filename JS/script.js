4b9b69463ac05941145964f20297e1522b8f83f3
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-02-06 21:40:42 +0100

    Finish creating the home page of the website

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,13 @@
 const navbarCollapse = document.querySelector("div.navbar-collapse");
+const pageFooter = document.querySelector("footer > p");
+
+const fullYear = new Date().getFullYear();
+
 
 document.addEventListener("click", () => {
   if (navbarCollapse.classList.contains("show")) {
     navbarCollapse.classList.remove("show");
   }  
 });
+
+pageFooter.innerHTML = `Copyright by <i class="fas fa-icicles text-primary"></i> Fro<span class="text-primary">st</span>Customs<sup>&copy;</sup> ${fullYear}`;
